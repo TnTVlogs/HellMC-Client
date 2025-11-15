5511e68726d0281a0c953bb1a1efd4d2748f9e62
Sergi <sergi.dalmau0303@gmail.com>
2025-11-15 10:56:23 +0100

    Distribution url fix & server IP change

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'http://hellmc.sytes.net/client/distribution.json'
+exports.REMOTE_DISTRO_URL = 'http://hellmc.sergidalmau.dev/client/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
