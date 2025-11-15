4c57fcff631793b450462ddf5a9204d4808a1e39
Sergi <sergi.dalmau0303@gmail.com>
2025-11-15 14:43:56 +0100

    Fix (again) distro url

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'http://hellmc.sergidalmau.dev/client/distribution.json'
+exports.REMOTE_DISTRO_URL = 'http://hellmcclient.sergidalmau.dev/client/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
